4ec7beb66ceef7d8c39b2d45e46a2e2a684a6ed1
vladimirow <vladimirow@mail.ru>
2010-06-19 20:17:44 +0000

    Resources updated

--- the-editor/trunk/Resource.h
+++ the-editor/trunk/Resource.h
@@ -2,18 +2,17 @@
 // Microsoft Visual C++ generated include file.
 // Used by Editor.rc
 //
-#define IDD_ABOUTBOX				100
-#define IDR_MAINFRAME				128
-#define IDR_UntitledTYPE				130
-#define ID_VIEW_LINE_NUMBERS             32771
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define ID_VIEW_LINE_NUMBERS            32771
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	310
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		310
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
