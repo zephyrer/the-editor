e729ed826fba6486142aac19d2e32602341b59d7
vladimirow <vladimirow@mail.ru>
2010-06-21 10:12:10 +0000

    Undo/redo buttons added

--- the-editor/trunk/Resource.h
+++ the-editor/trunk/Resource.h
@@ -5,20 +5,17 @@
 #define IDD_ABOUTBOX                    100
 #define IDR_MAINFRAME                   128
 #define ID_VIEW_LINE_NUMBERS            32771
-#define ID_EDIT_SELECTALL               32774
-#define ID_EDIT_CLEAR32775              32775
-#define ID_EDIT_FIND32776               32776
-#define ID_EDIT_REPLACE32777            32777
-#define ID_VIEW_WORDWRAP                32778
-#define ID_VIEW_FONT                    32779
-#define ID_HELP_DONATE                  32780
+#define ID_EDIT_SELECTALL               32772
+#define ID_VIEW_WORDWRAP                32773
+#define ID_VIEW_FONT                    32774
+#define ID_HELP_DONATE                  32775
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        310
-#define _APS_NEXT_COMMAND_VALUE         32781
+#define _APS_NEXT_COMMAND_VALUE         32776
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
