a9a4be91953a7c9ba222b1f23f7cfb6c7bf667c5
vladimirow@mail.ru <vladimirow@mail.ru@891533e0-f7a1-bf54-77a3-23a8049fc481>
2011-12-22 20:32:32 +0000

    Minor change

--- the-editor/trunk/editorctl/editorctl_offset_to_rc.c
+++ the-editor/trunk/editorctl/editorctl_offset_to_rc.c
@@ -29,35 +29,3 @@ BOOL editorctl_offset_to_rc (HWND hwnd, int offset, int *row, int *col)
 error:
     return FALSE;
 }
-
-/*
-BOOL editorctl_offset_to_rc (HWND hwnd, int offset, int *row, int *col)
-{
-    EDITORCTL_EXTRA *extra;
-    int r;
-    int c;
-    EDITORCTL_TEXT_ITERATOR it;
-
-    if ((extra = (EDITORCTL_EXTRA *)GetWindowLongPtr (hwnd, 0)) == NULL) goto error;
-
-    if (!editorctl_offset_to_row (hwnd, offset, &r)) goto error;
-
-    if (!editorctl_set_iterator (hwnd, extra->row_offsets [r], &it)) goto error;
-
-    c = 0;
-    while (it.offset < offset)
-    {
-        editorctl_forward (&it);
-        if (editorctl_get_prev_char (&it) == '\t')
-            c = (c + extra->tab_width) / extra->tab_width * extra->tab_width;
-        else c++;
-    }
-
-    *row = r;
-    *col = c;
-
-    return TRUE;
-error:
-    return FALSE;
-}
-*/
--- the-editor/trunk/editorctl/editorctl_rc_to_offset.c
+++ the-editor/trunk/editorctl/editorctl_rc_to_offset.c
@@ -36,36 +36,3 @@ BOOL editorctl_rc_to_offset (HWND hwnd, int row, int column, int *offset, int *r
 error:
     return FALSE;
 }
-
-/*
-BOOL editorctl_rc_to_offset (HWND hwnd, int row, int column, int *offset, int *real_col)
-{
-    EDITORCTL_EXTRA *extra;
-    EDITORCTL_TEXT_ITERATOR it;
-    int end_offset;
-    int c, o, rc;
-
-    if ((extra = (EDITORCTL_EXTRA *)GetWindowLongPtr (hwnd, 0)) == NULL) goto error;
-    if (!editorctl_set_iterator (hwnd, extra->row_offsets [row], &it)) goto error;
-    end_offset = row < extra->row_count - 1 ? extra->row_offsets [row + 1] : extra->text_length;
-    c = 0;
-    o = it.offset;
-    rc = 0;
-    while (c < column && it.offset < end_offset)
-    {
-        o = it.offset;
-        rc = c;
-
-        editorctl_forward (&it);
-        if (editorctl_get_prev_char (&it) == '\t')
-            c = (c + extra->tab_width) / extra->tab_width * extra->tab_width;
-        else c = c + 1;
-    }
-    *offset = o;
-    *real_col = rc;
-
-    return TRUE;
-error:
-    return FALSE;
-}
-*/
