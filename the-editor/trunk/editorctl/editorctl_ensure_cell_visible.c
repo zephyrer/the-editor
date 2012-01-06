da6acf445f3d91b8bd596f68968ab1648d059260
vladimirow@mail.ru <vladimirow@mail.ru@891533e0-f7a1-bf54-77a3-23a8049fc481>
2012-01-06 06:45:27 +0000

    Refactoring done

--- the-editor/trunk/editorctl/editorctl_ensure_cell_visible.c
+++ the-editor/trunk/editorctl/editorctl_ensure_cell_visible.c
@@ -1,33 +1,30 @@
 #include "editorctl.h"
 
-BOOL editorctl_ensure_cell_visible (HWND hwnd, int row, int col)
+BOOL editorctl_ensure_cell_visible (EDITORCTL *editorctl, int row, int col)
 {
-    EDITORCTL_EXTRA *extra;
     int x, y, sx, sy;
     RECT r;
 
-    if ((extra = (EDITORCTL_EXTRA *)GetWindowLongPtr (hwnd, 0)) == NULL) goto error;
+    x = col * editorctl->cell_size.cx;
+    y = row * editorctl->cell_size.cy;
+    sx = editorctl->scroll_location.x;
+    sy = editorctl->scroll_location.y;
 
-    x = col * extra->cell_size.cx;
-    y = row * extra->cell_size.cy;
-    sx = extra->scroll_location.x;
-    sy = extra->scroll_location.y;
+    if (!GetClientRect (editorctl->hwnd, &r)) goto error;
 
-    if (!GetClientRect (hwnd, &r)) goto error;
-
-    if (x + extra->cell_size.cx > r.right + sx)
-        sx = x + extra->cell_size.cx - r.right;
+    if (x + editorctl->cell_size.cx > r.right + sx)
+        sx = x + editorctl->cell_size.cx - r.right;
 
     if (x < r.left + sx)
         sx = x - r.left;
 
-    if (y + extra->cell_size.cy > r.bottom + sy)
-        sy = y + extra->cell_size.cy - r.bottom;
+    if (y + editorctl->cell_size.cy > r.bottom + sy)
+        sy = y + editorctl->cell_size.cy - r.bottom;
 
     if (y < r.top + sy)
         sy = y - r.top;
 
-    if (!editorctl_scroll_to (hwnd, sx, sy)) goto error;
+    if (!editorctl_scroll_to (editorctl, sx, sy)) goto error;
 
     return TRUE;
 error:
--- the-editor/trunk/editorctl/editorctl_update_caret_pos.c
+++ the-editor/trunk/editorctl/editorctl_update_caret_pos.c
@@ -9,6 +9,7 @@ BOOL editorctl_update_caret_pos (EDITORCTL *editorctl)
         row = layout_offset_to_row (&editorctl->editor.layout, editorctl->editor.caret_offset);
         col = layout_offset_to_column (&editorctl->editor.layout, editorctl->editor.caret_offset, row);
 
+        if (!editorctl_ensure_cell_visible (editorctl, row, col)) goto error;
         if (!SetCaretPos (col * editorctl->cell_size.cx - editorctl->scroll_location.x, row * editorctl->cell_size.cy - editorctl->scroll_location.y)) goto error;
     }
 
